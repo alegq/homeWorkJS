9a226cf4ff34b63c5c3390eb58d4054fb5d90cba
Oleg <yalovik678@gmail.com>
2022-03-22 11:18:12 +0300

    add Dragndrop_v3

--- Hw_7/DRAGNDROP.js
+++ Hw_7/DRAGNDROP.js
@@ -1,5 +1,8 @@
 var ImgEl=document.getElementsByTagName('img')
 
+let img = document.createElement('img');
+img.src = 'http://fe.it-academy.by/Sites/0000007/sax.jpg';
+
 
 imgPos()//изменяем позиционирование всех img
 function imgPos(){
@@ -8,6 +11,7 @@ function imgPos(){
     for (let i = 0; i<ImgEl.length; i++){
         imgTop[i]=ImgEl[i].offsetTop
         imgLeft[i]=ImgEl[i].offsetLeft
+        console.log('X:'+ imgLeft[i] + '   '+'Y: '+ imgTop[i])
     }
     for (let i = 0; i<ImgEl.length; i++){
         ImgEl[i].style.position='absolute';
@@ -26,7 +30,7 @@ function mousedownFun(EO){
     EO=EO||window.event;
 
     EO.target.style.cursor = "pointer";
-
+    var self = EO.target
     // найдём положение самой картинки относительно страницы
     var boxPos=getElementPos(EO.target);
 
@@ -55,7 +59,8 @@ function mousedownFun(EO){
         elem.style.zIndex = Number(z_max)+1;
     }
 
-    EO.target.addEventListener("mousemove",mousemoveFun,false)
+     //EO.target.addEventListener("mousemove",mousemoveFun,false)
+    document.body.addEventListener("mousemove",mousemoveFun,false)
 
     function mousemoveFun(EO){
         EO=EO||window.event;
@@ -65,13 +70,14 @@ function mousedownFun(EO){
         var clickY=Math.round(EO.pageY);
 
         // установим новыен координаты для картинки
-        EO.target.style.left=clickX -mousedownX +"px"
-        EO.target.style.top=clickY -mousedownY+"px";
+        self.style.left=clickX -mousedownX +"px"
+        self.style.top=clickY -mousedownY+"px";
     }
     
     document.body.addEventListener("mouseup",mouseupFun,false)
     function mouseupFun() {
-        EO.target.removeEventListener("mousemove",mousemoveFun,false)
+        //EO.target.removeEventListener("mousemove",mousemoveFun,false)
+        document.body.removeEventListener("mousemove",mousemoveFun,false)
         EO.target.style.cursor = "auto";
     }
 }
