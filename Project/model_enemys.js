bc5f6730a67440f7f06ee78064c3d5e90450192c
Oleg <yalovik678@gmail.com>
2022-04-12 15:42:15 +0300

    Creat Project

--- Project/model_enemys.js
+++ Project/model_enemys.js
@@ -1,5 +1,23 @@
 function Enemys_mod() {
     var state = 1;
+    this.move = function () {
+        // var SVGObjectElem=document.getElementById("green_going");
+        // var SVGDocument=SVGObjectElem.contentDocument;
+        // console.log(SVGDocument)
+        // var paths=SVGDocument.getElementsByTagName("path");
+
+        // console.log(SVGDocument.contentType)
+
+        var SVGObjectElem=document.getElementById("SSS");
+        var SVGDocument=SVGObjectElem.contentDocument;
+        console.log(SVGDocument);
+        var paths=SVGDocument.getElementsByTagName("path");
+        console.log(paths[0])
+
+
+    }
 }
 
+var f = new Enemys_mod()
+f.move()
 
